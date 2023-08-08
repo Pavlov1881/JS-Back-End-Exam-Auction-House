75aa6c33abd7dff676f3656aea37355f50127ce4
Anatoli Pavlov <18app81@gmail.com>
2023-08-08 16:34:13 +0300

    Update user model

--- models/User.js
+++ models/User.js
@@ -1,19 +1,29 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        minLength: [4, 'Username must be at least 4 characters long'],
-        required: [true, 'Username is required']
-    },
+
     email: {
         type: String,
         minLength: [10, 'Email must be at least 10 characters long'],
-        required: [true, 'Email is required']
+        required: [true, 'Email is required'],
+        validate: {
+            validator: (value) => /^[a-z]+@[a-z]+\.[a-z]+$/.test(value),
+            message: 'The email should be in the following format: <name>@<domain>.<extension> and only English letters!',
+        }
     },
     password: {
         type: String,
         required: [true, 'Password is required'],
+    },
+    firstName: {
+        type: String,
+        minLength: [1, 'First name must be at least 1 character long'],
+        required: [true, 'Username is required']
+    },
+    lastName: {
+        type: String,
+        minLength: [1, 'Last name must be at least 1 character long'],
+        required: [true, 'Username is required']
     },
 });
 
