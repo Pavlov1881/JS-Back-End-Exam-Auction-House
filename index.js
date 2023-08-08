2dae4dfa43923d7cd77a14d65740ea108d54304b
Anatoli Pavlov <18app81@gmail.com>
2023-08-08 16:34:47 +0300

    Add home page

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { authMiddleware } = require('./middlewares/authMiddleware');
 
-//! change DB name
-const databaseName = 'book-talk';
+const databaseName = 'auction-house';
 
 const routes = require('./routes');
 
@@ -15,8 +14,7 @@ app.engine('hbs', handlebars.engine({
 }));
 app.set('view engine', 'hbs');
 
-// app.use('/static', express.static('public'));
-app.use(express.static('public'));
+app.use('/static', express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
